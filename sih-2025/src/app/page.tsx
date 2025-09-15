49f9217800bada3053ef5682e6968c150b7751d7
Malavika05M <malavikamanju4@gmail.com>
2025-09-15 23:54:16 +0530

    feat:Initialize project

---
+++ sih-2025/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <p>
+        This is a simple Next.js page created with TypeScript.
+        You can start working from here.
+      </p>
+    </div>
+  );
+}
